c6a7f75ce4c4c8f1486303aacf4058f825e32ca9
Phat <phatdroid94@gmail.com>
2022-06-14 18:20:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/viewmodel29032022/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.viewmodel29032022;
+
+import android.Manifest;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvLocation;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvLocation = findViewById(R.id.text_view_location);
+
+        ActivityResultLauncher<String[]> locationPermissionRequest = registerForActivityResult(
+                new ActivityResultContracts.RequestMultiplePermissions(),
+                result -> {
+                    Boolean fineLocationGranted = result.getOrDefault(
+                            Manifest.permission.ACCESS_FINE_LOCATION, false);
+                    Boolean coarseLocationGranted = result.getOrDefault(
+                            Manifest.permission.ACCESS_COARSE_LOCATION, false);
+                    if (fineLocationGranted != null && fineLocationGranted) {
+                        Log.d("BBB", "Fine location accept");
+                    } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                        Log.d("BBB", "Coarse location accept");
+                    } else {
+                        // No location access granted.
+                    }
+                }
+        );
+
+        locationPermissionRequest.launch(new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
+    }
+}
